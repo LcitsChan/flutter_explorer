dc5f7796c4eb8be32ce16e47c4a9b949f308254d
LcitsChan <ctairong@gmail.com>
2021-03-19 23:10:52 +0800

    fix: freezed data model

--- lib/model/license.dart
+++ lib/model/license.dart
@@ -1,21 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:flutter/foundation.dart';
 
-part 'License.g.dart';
+part 'license.freezed.dart';
+part 'license.g.dart';
 
-@JsonSerializable()
-class License {
-  String key;
-  String name;
-  @JsonKey(name: 'spdx_id')
-  String spdxId;
-  String url;
-  @JsonKey(name: 'node_id')
-  String nodeId;
-
-  License(this.key, this.name, this.spdxId, this.url, this.nodeId);
-
+@freezed
+abstract class License with _$License {
+  const factory License({
+    String key,
+    String name,
+    @JsonKey(name: 'spdx_id')
+    String spdxId,
+    String url,
+    @JsonKey(name: 'node_id')
+    String nodeId
+}) = _License;
   factory License.fromJson(Map<String, dynamic> json) =>
       _$LicenseFromJson(json);
-
-  Map<String, dynamic> toJson() => _$LicenseToJson(this);
 }
